c713a175c3376139661af67175ec25731890ca78
Thejaswini6 <76125395+Thejaswini6@users.noreply.github.com>
2020-12-17 00:17:21 +0530

    Day 2 assignment submitted

---
+++ Day 2.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Try 5 Different functions of the String in Python.
+
+# In[1]:
+
+
+name = "Thejaswini N"
+
+
+# In[2]:
+
+
+name
+
+
+# In[6]:
+
+
+name.index("j")
+
+
+# In[9]:
+
+
+Class = "Vth sem"
+
+
+# In[10]:
+
+
+Class
+
+
+# In[11]:
+
+
+Class.split()
+
+
+# In[20]:
+
+
+Class.capitalize()
+
+
+# In[23]:
+
+
+name.join("na")
+
+
+# In[33]:
+
+
+Class.splitlines(1)
+
+
+# In[34]:
+
+
+Class.lower()
+
+
+# In[36]:
+
+
+Class.upper()
+
+
+# Try 5 Different functions of the List object in Python
+
+# In[62]:
+
+
+List = [1,2,3,4,"ABC",2.4,[10,32]]
+
+
+# In[63]:
+
+
+type(List)
+
+
+# In[64]:
+
+
+List
+
+
+# In[65]:
+
+
+List.append(23)
+
+
+# In[66]:
+
+
+List
+
+
+# In[67]:
+
+
+List.count("ABC")
+
+
+# In[68]:
+
+
+List.index(2.4)
+
+
+# In[69]:
+
+
+List.remove(4)
+
+
+# In[70]:
+
+
+List
+
+
+# In[72]:
+
+
+List.reverse()
+
+
+# In[73]:
+
+
+List
+
+
+# Experiment with at least 5 default functions of Dictionary
+
+# In[74]:
+
+
+Dit = {"key":"value", "key1":1, "key2":3.8}
+
+
+# In[75]:
+
+
+Dit
+
+
+# In[76]:
+
+
+Dit.get("key")
+
+
+# In[79]:
+
+
+Dit.copy()
+
+
+# In[80]:
+
+
+Dit.values()
+
+
+# In[103]:
+
+
+Dit.setdefault(0)
+
+
+# In[104]:
+
+
+Dit.setdefault(1)
+
+
+# In[105]:
+
+
+Dit
+
+
+# In[106]:
+
+
+Dit.update({"name": "Krish"})
+
+
+# In[107]:
+
+
+Dit
+
+
+# In[ ]:
+
+
+
+
